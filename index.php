f8106a2628ad206fc759e347fe4863fb3eb8be35
i-gofman <igofman@gmail.com>
2017-07-26 17:03:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="images/logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:" Labs users")."!"; ?></h1>
+	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:" Labs users !!!")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
